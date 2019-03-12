87cabf887b4016bbc1548206835f024ac6255493
孙杨 <956086341@qq.com>
2019-03-12 10:54:12 +0800

    相关文件

---
+++ study-jenkinsK8s/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+serverName=study-jenkinsK8s
+workDir=/mnt/k8sApp/$serverName
+imageRepertory=47.94.159.51/dev
+serverVersion=latest
+
+
+
+imageName=$serverName_image
+
+function buildImage(){
+    docker image build -t $imageName:$serverVersion $workDir
+}
+
+function pushImage(){
+    docker tag $imageName:$serverVersion $imageRepertory/$imageName:$serverVersion
+    docker push $imageRepertory/$imageName:$serverVersion
+}
+
+function k8sDeploy(){
+    kubectl apply -f $serverName.yaml
+}
+
+buildImage
+
+pushImage
+
+k8sDeploy
