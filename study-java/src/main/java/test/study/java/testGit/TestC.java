cd8ebc39ab1987185059b26f685ceedf1ab4a245
sunyang <sunyang@uino.com>
2023-06-26 16:27:02 +0800

    feat: 新写了一个问题

---
+++ study-java/src/main/java/test/study/java/testGit/TestC.java
@@ -0,0 +1,15 @@
+package test.study.java.testGit;
+
+/**
+ * @author sunYang
+ * @date 2023/6/26 16:25
+ */
+public class TestC {
+
+    TestA ta = new TestA();
+
+    public void cc(){
+        System.out.println(ta.c());
+    }
+
+}
